eb9f9b89a58a981cc12c35d5c5a012e43dc19411
hiteshpatel98 <113504215+hiteshpatel98@users.noreply.github.com>
2023-01-16 19:19:24 +0530

    added header in dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../../components/header/Header";
 
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Header></Header>
     </div>
   );
 };
--- src/resources/constants.tsx
+++ src/resources/constants.tsx
@@ -1,2 +1,4 @@
 export const logo = `${process.env.REACT_APP_CDN_URL}/images/logo.png`;
 export const ksb_logo = `${process.env.REACT_APP_CDN_URL}/images/ksb_logo.png`;
+export const ksb_icon = `${process.env.REACT_APP_CDN_URL}/images/ksb-icon.png`;
+export const user_avatar_icon = `${process.env.REACT_APP_CDN_URL}/images/user-icon.png`;
