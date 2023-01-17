f59c9be9f3c13bc073c58a1fbd88b3e39018ef55
hiteshpatel98 <113504215+hiteshpatel98@users.noreply.github.com>
2023-01-17 10:37:49 +0530

    added basic structure for dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../../components/header/Header";
+import CustomTable from "../../components/table/CustomTable";
+import { jsonData } from "./sample-data";
 
 const Dashboard = () => {
   return (
     <div>
       <Header></Header>
+      <CustomTable data={jsonData}></CustomTable>
     </div>
   );
 };
